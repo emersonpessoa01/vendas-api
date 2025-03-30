4d6590c633694a25d3bd6a3a31fb430a01b02571
emersonpessoa01 <eps011108@gmail.com>
2025-03-30 18:08:02 -0300

    first commit

---
+++ src/test/java/io/github/emersonpessoa01/vendas_api/VendasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emersonpessoa01.vendas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
