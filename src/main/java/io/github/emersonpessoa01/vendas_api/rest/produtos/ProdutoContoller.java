a6c4e00a998ac9db87a95c2fbe3dd11d32206efe
emersonpessoa01 <eps011108@gmail.com>
2025-03-30 20:08:09 -0300

    feat: Creating the product post :rocket:

---
+++ src/main/java/io/github/emersonpessoa01/vendas_api/rest/produtos/ProdutoContoller.java
@@ -0,0 +1,17 @@
+package io.github.emersonpessoa01.vendas_api.rest.produtos;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/produtos")
+
+public class ProdutoContoller {
+    @PostMapping("/")
+    public ProdutoFormRequest salvar(@RequestBody ProdutoFormRequest produto) {
+        System.out.println(produto +": Produto salvo com sucesso!");
+        return produto; // Retorna o produto com os dados preenchidos
+    }
+}
