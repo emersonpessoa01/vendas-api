adb6ed00645d35295bfce8ef983765148e9d12aa
emersonpessoa01 <eps011108@gmail.com>
2025-04-02 11:48:45 -0300

    feat: Saving product in the database :sparkles:

--- src/main/java/io/github/emersonpessoa01/vendas_api/model/Produto.java
+++ src/main/java/io/github/emersonpessoa01/vendas_api/model/Produto.java
@@ -27,19 +27,93 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Lembrando que não é necessário colocar o @Column em todos os atributos, pois
+    // o JPA já faz isso por padrão.
     @Column(name = "nome", length = 100, nullable = false)
     private String nome;
     @Column(name = "descricao", length = 255, nullable = false)
     private String descricao;
-    @Column(name = "preco", precision = 2, scale = 16, nullable = false)
+    @Column(name = "preco", precision = 16, scale = 2, nullable = false)
     private BigDecimal preco;
     @Column(name = "sku", length = 20, nullable = false, unique = true)
     private String sku;
+
+
 
     @Override
     public String toString() {
         return "Produto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", preco=" + preco + ", sku=" + sku
                 + "]";
+    }
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return String return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return BigDecimal return the preco
+     */
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return String return the sku
+     */
+    public String getSku() {
+        return sku;
+    }
+
+    /**
+     * @param sku the sku to set
+     */
+    public void setSku(String sku) {
+        this.sku = sku;
     }
 
 }
--- src/main/java/io/github/emersonpessoa01/vendas_api/rest/produtos/ProdutoController.java
+++ src/main/java/io/github/emersonpessoa01/vendas_api/rest/produtos/ProdutoController.java
@@ -1,17 +1,33 @@
 package io.github.emersonpessoa01.vendas_api.rest.produtos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.emersonpessoa01.vendas_api.model.Produto;
+import io.github.emersonpessoa01.vendas_api.model.repository.ProdutoRepository;
+
 @RestController
 @RequestMapping("/api/produtos")
 
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     @PostMapping
     public ProdutoFormRequest salvar(@RequestBody ProdutoFormRequest produto) {
-        System.out.println(produto +": Produto salvo com sucesso!");
+        Produto entidadeProduto = new Produto(null, produto.getNome(), produto.getDescricao(), produto.getPreco(),
+                produto.getSku());
+
+        // Salva o produto no banco de dados
+        produtoRepository.save(entidadeProduto);
+
+        System.out.println("Imprimindo Entidade Produto" + entidadeProduto);
+
+        // System.out.println(produto +": Produto salvo com sucesso!");
         return produto; // Retorna o produto com os dados preenchidos
     }
 }
