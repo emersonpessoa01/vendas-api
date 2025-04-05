76e57e8b806ae45276914ab5740d072e6939d94a
emersonpessoa01 <eps011108@gmail.com>
2025-04-04 21:18:14 -0300

    feat: Added CORS :sparkles:

--- src/main/java/io/github/emersonpessoa01/vendas_api/rest/produtos/ProdutoController.java
+++ src/main/java/io/github/emersonpessoa01/vendas_api/rest/produtos/ProdutoController.java
@@ -1,6 +1,7 @@
 package io.github.emersonpessoa01.vendas_api.rest.produtos;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import io.github.emersonpessoa01.vendas_api.model.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
+@CrossOrigin(origins = "*")
 
 public class ProdutoController {
 
